fdfb27a5dbab4268e5b970e8c9bc73102a45e727
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-07-18 16:06:52 +0300

    Updated the JS with checks

--- assets/js/akan-name.js
+++ assets/js/akan-name.js
@@ -1,20 +1,51 @@
+
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "md-toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": 300,
+    "hideDuration": 1000,
+    "timeOut": 5000,
+    "extendedTimeOut": 1000,
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+
+  
 let getInputValues = (formDate, formYear, forMonth, formGender) =>{
     formDate = document.getElementById("form-Date").value;
     formYear = document.getElementById("form-Year").value;
     forMonth = document.getElementById("month-Select").value;
     formGender = document.querySelector('input[name=groupOfMaterialRadios]:checked').value;
     console.log(formDate, formYear, forMonth, formGender);
-    
-    let akaName = () => {
+    let dataCheck = () => {
         let birthYear = parseInt(formYear);
-        let birthMonth = parseInt(formDate);
-        console.log(birthYear, birthMonth)
+        let birthMonth = parseInt(forMonth);
+        let birthDate = parseInt(formDate);
+        if (birthYear ==null || birthYear ==""){
+            alert("Please enter a valid birth year")
+            return false;
+        }
+
+    }
+    dataCheck()
+    let akaName = () => {
+       
+        //console.log(birthYear, birthMonth)
     
-        let day = birthYear
+        // let day = birthYear
     
     }
     akaName()
+    toastr.success("Form submit successfully!", "Success");
+
 
 }
-
+toastr.success("Form submit successfully!", "Success");
 
