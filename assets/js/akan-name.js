c6158e40d32f882902f08984ad1dea909d16260f
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-07-18 18:04:08 +0300

    Updated the JS to check to return Akan name

--- assets/js/akan-name.js
+++ assets/js/akan-name.js
@@ -26,19 +26,18 @@ let getInputValues = (formDate, formYear, forMonth, formGender) =>{
     console.log(formDate, formYear, forMonth, formGender);
     let dataCheck = () => {
         let birthYear = parseInt(formYear);
-        let birthMonth = parseInt(forMonth);
         let birthDate = parseInt(formDate);
         if (birthYear ==null || birthYear ==""){
             alert("Please enter a valid birth year")
             return false;
-        }else if (birthYear.toString().length<4 || birthYear > 2021) {
+        }else if (birthYear.toString().length<4 || birthYear < 1000|| birthYear > 2021) {
             alert("Please enter a valid year")
             return false;
         }
         if (birthDate ==null || birthDate ==""){
             alert("Please enter a valid birth year")
             return false;
-        } else if (birthDate.toString().length > 2 || birthdate > 31) {
+        } else if (birthDate.toString().length > 2 || birthDate < 0|| birthDate > 31) { 
             alert("Please enter a valid date")
         }
 
@@ -46,15 +45,46 @@ let getInputValues = (formDate, formYear, forMonth, formGender) =>{
 
 
     }
-    dataCheck()
     let akaName = () => {
-       
+        let newBirthYear = Array.from(formYear.toString()).map(Number);
+        let dd = parseInt(formDate);
+        let birthYear = parseInt(formYear);
+        let birthMonth = parseInt(forMonth);
+        console.log(newBirthYear);
+        cc = newBirthYear.slice(0,2);
+        yy = newBirthYear.slice(1,);
+        let nickName = String;
+
+        // day = (((cc/4) - 2*cc-1)+ 1 ((5*yy/4)) + ((26*(birthMonth+1)/10))+ dd) % 7;
+        d = new Date(birthYear, birthMonth, dd);
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        day = days[d.getDay()];
+
+        console.log(day); 
+        if (formGender == "male" ){
+            if (day == "Sunday") {
+                nickName = "Kwasi";
+            }else if (day == "Monday") {
+                nickName = "Kwadwo";
+            }else if (day == "Tuesday") {
+                nickName = "Kwabena";
+            }else if (day == "Wednesday") {
+                nickName = "Kwaku"
+            }else if (day == "Thursday") {
+                nickName = "Yaw"
+            }else if (day == "Friday") {
+                nickName = "Kofi"
+            }else if (day == "Saturday"){
+                nickName = "Kwame"
+            }
+
+        }
     
-        // let day = birthYear
+        console.log(nickName);
     
     }
     akaName()
-    toastr.success("Form submit successfully!", "Success");
+    dataCheck()
 
 
 }
