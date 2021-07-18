c90bdb19cea72d944dd871684db934add3710ee0
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-07-18 12:02:00 +0300

    Addded js function to get input values

--- assets/js/akan-name.js
+++ assets/js/akan-name.js
@@ -1,3 +1,11 @@
+let getInputValues = ()=>{
+    formDate = document.getElementById("form-Date");
+    formYear = document.getElementById("form-Year");
+    console.log(formDate, formYear);
+
+}
+
+
 let akaName = (birthYear, birthDate, birthMonth, gender) => {
     let day = birthYear
 
